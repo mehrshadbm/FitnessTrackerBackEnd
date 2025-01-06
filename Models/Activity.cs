5b908dc865a772836b9417a0534fa00fc2e27464
Mehrshad Bahmani <mehrshadbahmani79@gmail.com>
2025-01-06 16:23:44 +1100

    added activity model

---
+++ Models/Activity.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FitnessTrackerBackend.Models
+{
+    public class Activity
+    {
+
+        public Guid Id {  get; set; }
+        [Required]
+        public string ActivityType { get; set; }
+        [Required]
+        public int Duration { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public IntensityLevel Intensity {  get; set; }
+        
+    }
+
+
+    public enum IntensityLevel
+    {
+        Easy = 1,
+        Moderate = 2,
+        Hard = 3,
+        AllOut = 4
+    }
+}
